d0d24b2e7f74e5b7e7895900c902611377a7dfca
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-04-21 10:44:03 -0300

    :test_tube: test: Testando a importação da API

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,36 @@
 import { useState, useEffect } from "react"
 
 function Home() {
+
+    const [dadosCovid, setDadosCovid] = useState([])
+
+    useEffect(() => {
+        fetch(`https://disease.sh/v3/covid-19/vaccine/coverage/countries?lastdays=1`)
+        .then(response => response.json())
+        .then(data => setDadosCovid(data.results))
+    }, [])
+
     return(
         <section>
+
+            <h1>Casos</h1>
+
+            {dadosCovid.map((dados) => {
+                const {pais, timeline} = dados
+                return (
+                    <ul>
+                        <li>
+                            <h2>`${dados.id}`</h2>
+                        </li>
+                        <li>
+                            {dados.pais}
+                        </li>
+                        <li>
+                            {dados.timeline}
+                        </li>
+                    </ul>
+                )
+            })}
         </section>
     )
 }
