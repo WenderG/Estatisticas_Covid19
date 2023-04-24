6f4c051fb354125e61c493003fd68c8f74d181bc
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-04-24 15:02:09 -0300

    :books: docs: Modificações

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,11 @@
-import React, { useRef, useEffect } from "react";
-import PropTypes, { func } from "prop-types";
+import React from "react";
 import { Helmet } from "react-helmet";
-import L from "leaflet";
-import { Marker, useMap } from "react-leaflet";
-
-import { promiseToFlyTo, getCurrentLocation } from "lib/map";
+import { Marker } from "react-leaflet";
 
 import Layout from "components/Layout";
 import Container from "components/Container";
 import Map from "components/Map";
 import Snippet from "components/Snippet";
-
-import gatsby_astronaut from "assets/images/gatsby-astronaut.jpg";
 
 import { useTracker } from 'hooks'
 
@@ -21,72 +15,6 @@ const LOCATION = {
 };
 const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
-const ZOOM = 10;
-
-const timeToZoom = 2000;
-const timeToOpenPopupAfterZoom = 4000;
-const timeToUpdatePopupAfterZoom = timeToOpenPopupAfterZoom + 3000;
-
-const popupContentHello = `<p>Hello 👋</p>`;
-const popupContentGatsby = `
-  <div class="popup-gatsby">
-    <div class="popup-gatsby-image">
-      <img class="gatsby-astronaut" src=${gatsby_astronaut} />
-    </div>
-    <div class="popup-gatsby-content">
-      <h1>Gatsby Leaflet Starter</h1>
-      <p>Welcome to your new Gatsby site. Now go build something great!</p>
-    </div>
-  </div>
-`;
-
-/**
- * MapEffect
- * @description This is an example of creating an effect used to zoom in and set a popup on load
- */
-
-const MapEffect = ({ markerRef }) => {
-  const map = useMap();
-
-  useEffect(() => {
-    if (!markerRef.current || !map) return;
-
-    (async function run() {
-      const popup = L.popup({
-        maxWidth: 800,
-      });
-
-      const location = await getCurrentLocation().catch(() => LOCATION);
-
-      const { current: marker } = markerRef || {};
-
-      marker.setLatLng(location);
-      popup.setLatLng(location);
-      popup.setContent(popupContentHello);
-
-      setTimeout(async () => {
-        await promiseToFlyTo(map, {
-          zoom: ZOOM,
-          center: location,
-        });
-
-        marker.bindPopup(popup);
-
-        setTimeout(() => marker.openPopup(), timeToOpenPopupAfterZoom);
-        setTimeout(
-          () => marker.setPopupContent(popupContentGatsby),
-          timeToUpdatePopupAfterZoom
-        );
-      }, timeToZoom);
-    })();
-  }, [map, markerRef]);
-
-  return null;
-};
-
-MapEffect.propTypes = {
-  markerRef: PropTypes.object,
-};
 
 const IndexPage = () => {
 
@@ -94,7 +22,9 @@ const IndexPage = () => {
     api: 'countries'
   })
 
-  console.log('countries', countries)
+  const  { data: stats = [] } = useTracker({
+    api: 'all'
+  })
 
   const hasCountries = Array.isArray(countries) && countries.length > 0
 
@@ -120,26 +50,83 @@ const IndexPage = () => {
     }
   }
 
-  const markerRef = useRef();
-
   const mapSettings = {
     center: CENTER,
     defaultBaseMap: "OpenStreetMap",
     zoom: DEFAULT_ZOOM,
   };
 
+  const dashboardStats = [
+    {
+      primary: {
+        label: 'Total Cases',
+        value: stats?.Cases
+      },
+
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats?.casesPerOneMillion
+      },
+
+    },
+
+    {
+      primary: {
+        label: 'Total Deaths',
+        value: stats?.deaths
+      },
+
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats?.deathsPerOneMillion
+      },
+    },
+
+    {
+      primary: {
+        label: 'Total Tests',
+        value: stats?.tests
+      },
+
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats?.testsPerOneMillion
+      },
+    },
+
+    {
+      primary: {
+        label: 'Active Cases',
+        value: stats?.active
+      },
+    },
+
+    {
+      primary: {
+        label: 'Critical Cases',
+        value: stats?.critical
+      },
+    },
+
+    {
+      primary: {
+        label: 'Recovered Cases',
+        value: stats?.recovered
+      },
+    }
+  ]
+
   return (
     <Layout pageName="home">
       <Helmet>
-        <title>Home Page</title>
+        <title>Estatística Covid-19</title>
       </Helmet>
 
       <Map {...mapSettings}>
-        <MapEffect markerRef={markerRef} />
-        <Marker ref={markerRef} position={CENTER} />
+        <Marker position={CENTER} />
       </Map>
 
-      <Container type="content" className="text-center home-start">
+      {/* <Container type="content" className="text-center home-start">
         <h2>Still Getting Started?</h2>
         <p>Run the following in your terminal!</p>
         <Snippet>
@@ -149,7 +136,7 @@ const IndexPage = () => {
         <p className="note">
           Note: Gatsby CLI required globally for the above command
         </p>
-      </Container>
+      </Container> */}
     </Layout>
   );
 };
