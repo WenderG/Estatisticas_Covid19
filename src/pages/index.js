5991baf4e73d88761ec4d16de0e50a9f6b2aed5d
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-04-25 15:13:25 -0300

    :books: docs: Modificações

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import { Helmet } from "react-helmet";
-import { Marker } from "react-leaflet";
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import L from 'leaflet';
 
-import Layout from "components/Layout";
-import Container from "components/Container";
-import Map from "components/Map";
-import Snippet from "components/Snippet";
+import { useTracker } from 'hooks';
+import { commafy, friendlyDate } from 'lib/util';
 
-import { useTracker } from 'hooks'
+import Layout from 'components/Layout';
+import Container from 'components/Container';
+import Map from 'components/Map';
 
 const LOCATION = {
   lat: 0,
@@ -17,25 +17,80 @@ const CENTER = [LOCATION.lat, LOCATION.lng];
 const DEFAULT_ZOOM = 2;
 
 const IndexPage = () => {
+  const { data: stats = {} } = useTracker({
+    api: 'all',
+  });
 
-  const  { data: countries = [] } = useTracker({
-    api: 'countries'
-  })
+  const { data: countries = [] } = useTracker({
+    api: 'countries',
+  });
 
-  const  { data: stats = [] } = useTracker({
-    api: 'all'
-  })
+  const hasCountries = Array.isArray( countries ) && countries.length > 0;
 
-  const hasCountries = Array.isArray(countries) && countries.length > 0
+  const dashboardStats = [
+    {
+      primary: {
+        label: 'Total Cases',
+        value: stats ? commafy( stats?.cases ) : '-',
+      },
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats ? commafy( stats?.casesPerOneMillion ) : '-',
+      },
+    },
+    {
+      primary: {
+        label: 'Total Deaths',
+        value: stats ? commafy( stats?.deaths ) : '-',
+      },
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats ? commafy( stats?.deathsPerOneMillion ) : '-',
+      },
+    },
+    {
+      primary: {
+        label: 'Total Tests',
+        value: stats ? commafy( stats?.tests ) : '-',
+      },
+      secondary: {
+        label: 'Per 1 Million',
+        value: stats ? commafy( stats?.testsPerOneMillion ) : '-',
+      },
+    },
+    {
+      primary: {
+        label: 'Active Cases',
+        value: stats ? commafy( stats?.active ) : '-',
+      },
+    },
+    {
+      primary: {
+        label: 'Critical Cases',
+        value: stats ? commafy( stats?.critical ) : '-',
+      },
+    },
+    {
+      primary: {
+        label: 'Recovered Cases',
+        value: stats ? commafy( stats?.recovered ) : '-',
+      },
+    },
+  ];
 
-  async function mapEffect({ leafletElement: map} = {}) {
-    if(!hasCountries) return
+  async function mapEffect({ leafletElement: map } = {}) {
+    if ( !hasCountries || !map ) return;
+
+    map.eachLayer(( layer ) => {
+      if ( layer?.options?.name === 'OpenStreetMap' ) return;
+      map.removeLayer( layer );
+    });
 
     const geoJson = {
       type: 'FeatureCollection',
-      features: countries.map((country = {}) => {
-        const { countryInfo = {} } = country
-        const { lat, long: lng } = countryInfo
+      features: countries.map(( country = {}) => {
+        const { countryInfo = {} } = country;
+        const { lat, long: lng } = countryInfo;
         return {
           type: 'Feature',
           properties: {
@@ -43,100 +98,101 @@ const IndexPage = () => {
           },
           geometry: {
             type: 'Point',
-            coordinates: [lng, lat]
-          }
+            coordinates: [lng, lat],
+          },
+        };
+      }),
+    };
+
+    const geoJsonLayers = new L.GeoJSON( geoJson, {
+      pointToLayer: ( feature = {}, latlng ) => {
+        const { properties = {} } = feature;
+        let updatedFormatted;
+        let casesString;
+
+        const { country, updated, cases, deaths, recovered } = properties;
+
+        casesString = `${cases}`;
+
+        if ( cases > 1000000 ) {
+          casesString = `${casesString.slice( 0, -6 )}M+`;
+        } else if ( cases > 1000 ) {
+          casesString = `${casesString.slice( 0, -3 )}K+`;
         }
-      })
-    }
+        if ( updated ) {
+          updatedFormatted = new Date( updated ).toLocaleString();
+        }
+
+        const html = `
+          <span class="icon-marker">
+            <span class="icon-marker-tooltip">
+              <h2>${country}</h2>
+              <ul>
+                <li><strong>Confirmed:</strong> ${cases}</li>
+                <li><strong>Deaths:</strong> ${deaths}</li>
+                <li><strong>Recovered:</strong> ${recovered}</li>
+                <li><strong>Last Update:</strong> ${updatedFormatted}</li>
+              </ul>
+            </span>
+            ${casesString}
+          </span>
+        `;
+
+        return L.marker( latlng, {
+          icon: L.divIcon({
+            className: 'icon',
+            html,
+          }),
+          riseOnHover: true,
+        });
+      },
+    });
+
+    geoJsonLayers.addTo( map );
   }
 
   const mapSettings = {
     center: CENTER,
-    defaultBaseMap: "OpenStreetMap",
+    defaultBaseMap: 'OpenStreetMap',
     zoom: DEFAULT_ZOOM,
+    mapEffect,
   };
-
-  const dashboardStats = [
-    {
-      primary: {
-        label: 'Total Cases',
-        value: stats?.Cases
-      },
-
-      secondary: {
-        label: 'Per 1 Million',
-        value: stats?.casesPerOneMillion
-      },
-
-    },
-
-    {
-      primary: {
-        label: 'Total Deaths',
-        value: stats?.deaths
-      },
-
-      secondary: {
-        label: 'Per 1 Million',
-        value: stats?.deathsPerOneMillion
-      },
-    },
-
-    {
-      primary: {
-        label: 'Total Tests',
-        value: stats?.tests
-      },
-
-      secondary: {
-        label: 'Per 1 Million',
-        value: stats?.testsPerOneMillion
-      },
-    },
-
-    {
-      primary: {
-        label: 'Active Cases',
-        value: stats?.active
-      },
-    },
-
-    {
-      primary: {
-        label: 'Critical Cases',
-        value: stats?.critical
-      },
-    },
-
-    {
-      primary: {
-        label: 'Recovered Cases',
-        value: stats?.recovered
-      },
-    }
-  ]
 
   return (
     <Layout pageName="home">
       <Helmet>
-        <title>Estatística Covid-19</title>
+        <title>Estatisticas do Covid-19</title>
       </Helmet>
 
-      <Map {...mapSettings}>
-        <Marker position={CENTER} />
-      </Map>
+      <div className="tracker">
+        <Map {...mapSettings} />
+        <div className="tracker-stats">
+          <ul>
+            { dashboardStats.map(({ primary = {}, secondary = {} }, i ) => {
+              return (
+                <li key={`Stat-${i}`} className="tracker-stat">
+                  { primary.value && (
+                    <p className="tracker-stat-primary">
+                      { primary.value }
+                      <strong>{ primary.label }</strong>
+                    </p>
+                  ) }
+                  { secondary.value && (
+                    <p className="tracker-stat-secondary">
+                      { secondary.value }
+                      <strong>{ secondary.label }</strong>
+                    </p>
+                  ) }
+                </li>
+              );
+            }) }
+          </ul>
+        </div>
+        <div className="tracker-last-updated">
+          <p>Last Updated: { stats ? friendlyDate( stats?.updated ) : '-' }</p>
+        </div>
+      </div>
 
-      {/* <Container type="content" className="text-center home-start">
-        <h2>Still Getting Started?</h2>
-        <p>Run the following in your terminal!</p>
-        <Snippet>
-          gatsby new [directory]
-          https://github.com/colbyfayock/gatsby-starter-leaflet
-        </Snippet>
-        <p className="note">
-          Note: Gatsby CLI required globally for the above command
-        </p>
-      </Container> */}
     </Layout>
   );
 };
